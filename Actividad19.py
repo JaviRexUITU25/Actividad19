f647656d5608837dbc7d7cb4b7f8e5738494b130
André <javiandre2007@gmail.com>
2025-08-12 21:47:46 -0600

    agregar opcion y menu

--- Actividad19.py
+++ Actividad19.py
@@ -4,12 +4,13 @@ class Cookie:
         self.weight = weight
     def show_info(self):
         print(f"La galleta tiene un precio de: {self.price}, y tiene un peso de {self.weight}")
-
+cookies = []
 class ChipCookie(Cookie):
     def __init__(self, cantidad_chispas):
         self.cantidad_chispas = cantidad_chispas
     def show_info(self):
-        print(f"La galleta ahora tiene{self.cantidad_chispas} chispas")
+        print(f"La galleta con el precio de: {self.price} quetzaltes y peso de {self.weight}\n"
+              f" ahora tiene{self.cantidad_chispas} chispas")
 
 class RellenoCookie(Cookie, ChipCookie):
     def __init__(self, sabor_relleno):
@@ -17,3 +18,28 @@ class RellenoCookie(Cookie, ChipCookie):
     def describir_relleno(self):
         print(f"La galleta el precio de: {self.price} quetzales, y  peso de {self.weight} y \n"
               f"{self.cantidad_chispas} chispas, tiene un releno de {self.sabor_relleno}")
+
+def hi():
+    print("-"*10 + "BIENVENIDO AL REGISTRO DE GALLETAS" + "-"*10)
+
+def options():
+    try:
+        print("1.Registrar galleta básica. \n"
+              "2. Registrar galleta con chispas \n"
+              "3. Registrar galleta rellena\n"
+              "4. Lista de galletas\n"
+              "5. Buscar galleta por nombre\n"
+              "6. Eliminar galleta por nombre\n"
+              "7. Salir del programa")
+        option = int(input("Ingrese la opcion que desea:"))
+        match option:
+            case "1":
+                print()
+
+    except ValueError:
+        print("Ingrese un dato valido")
+    except TypeError:
+        print("Ingrese un numero")
+    except Exception as e:
+        print("Un error inesperado ha ocurrido", e)
+
