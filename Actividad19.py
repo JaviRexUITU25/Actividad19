518b64518874df8fcdd6cf7443a353bf0d3350df
Javier Huertas <javiandre2007@gmail.com>
2025-08-13 12:21:01 -0600

    agregue las primeras opciones y verificar que la galleta ha sido agregada

--- Actividad19.py
+++ Actividad19.py
@@ -1,44 +1,63 @@
 class Cookie:
-    def __init__(self,name,price, weight):
+    def __init__(self, name, price, weight):
         self.name = name
         self.price = price
         self.weight = weight
     def show_info(self):
-        print(f"La galleta tiene un precio de: {self.price}, y tiene un peso de {self.weight}")
-class ChipCookie(Cookie):
-    def __init__(self, chips_amount):
-        super().__init__(name, price, weight)
-        self.chips_amount = chips_amount
-        try:
-            if chips_amount <= 0 :
-                print("La cantidad de chispas debe ser un numero mayor a 0")
-        except ValueError:
-            print("Ingrese un valor valido")
-        except TypeError:
-            print("Ingrese un tipo de valor valido")
-        except Exception as e:
-            print("Un error inesperado ha ocurrido")
-    def show_info(self):
-        print(f"La galleta con el precio de: {self.price} quetzaltes y peso de {self.weight}\n"
-              f" ahora tiene{self.chips_amount} chispas")
+        print(f"La galleta {self.name} tiene un precio de: {self.price}, y tiene un peso de {self.weight}")
 
 class Relleno:
     def __init__(self, sabor_relleno):
         self.sabor_relleno = sabor_relleno
     def describir_relleno(self):
-        relleno = input("Describa que relleno lleva la galleta: ")
+        print(f"El relleno que lleva la galleta es: {self.sabor_relleno}")
+
+class ChipCookie(Cookie):
+    def __init__(self, name, price, weight, chips_amount):
+        super().__init__(name, price, weight)
+        self.chips_amount = chips_amount
 
 class FilledCookie(Cookie, Relleno):
-    def __init__(self, filled_flavor):
+    def __init__(self, name, price, weight, filled_flavor):
         super().__init__(name, price, weight)
         self.filled_flavor = filled_flavor
-
-def describir_relleno(self):
-        print(f"La galleta el precio de: {self.price} quetzales, y  peso de {self.weight} y \n"
-              f"{self.chips_amount} chispas, tiene un releno de {self.filled_flavor}")
+    def show_info(self):
+        print(f"La galleta {self.name} tiene un precio de: {self.price} quetzales, y tiene un peso de {self.weight} lb")
+        print(f"la galleta tiene un relleno de: {self.filled_flavor}")
 
 class data_filled:
     def __init__(self):self.cookies = []
+    def addedCookie(self):
+        while True:
+            name2 = input("Ingrese el nombre de la galleta: ")
+            if name2 == " ": break
+                print("Nombre vacio, debe agregar una galleta primero.")
+        while True:
+            price2 = int(input("Ingrese el precio de la galleta: "))
+            if price2 <0: break
+            print("El precio debe ser mayor a 0")
+        while True:
+            peso2 = int(input("Ingrese el peso de la galleta"))
+            if peso2 <0: break
+            print("El precio de la galleta debe ser mayor a 0 ")
+
+    def add_cookie(self):
+        try:
+            name, price, weight = self.
+            _new = Cookie(name, price, weight)
+            cookies.append(_new)
+            print(f"Galleta {name} ha sido agregada correctamente!")
+            if not name:
+                print("El nombre esta vacio")
+            elif price < 0:
+                print("El precio debe ser mayor a 0")
+            elif weight < 0:
+                print("El peso debe ser mayor a 0")
+
+        except Exception as e:
+            print("Un error inesperado ha ocurrido!\n"
+                  "Error: ", e)
+
 def hi():
     print("-"*10 + "BIENVENIDO AL REGISTRO DE GALLETAS" + "-"*10)
 
